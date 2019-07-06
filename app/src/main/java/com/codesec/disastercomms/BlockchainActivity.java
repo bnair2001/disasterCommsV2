f39d26a4e2b2bc9b5b118667a7264cdfe79f3897
Bharath <bharathrajeevnair@gmail.com>
2019-07-06 23:02:45 +0530

    blockchain saves as json
    blockchain saves as json file in internal storage ready for upload

--- app/src/main/java/com/codesec/disastercomms/BlockchainActivity.java
+++ app/src/main/java/com/codesec/disastercomms/BlockchainActivity.java
@@ -1,24 +1,50 @@
 package com.codesec.disastercomms;
 
 import android.content.SharedPreferences;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
 
 public class BlockchainActivity extends AppCompatActivity {
 
     SharedPreferences pref;
     TextView display;
-
+    String disaster="blockchain";
+    String obj;
+    String pathSDCard = Environment.getExternalStorageDirectory() + "/Android/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blockchain);
+
         pref = getSharedPreferences("settings", 0);
         display = (TextView) findViewById(R.id.textView2);
         display.setMovementMethod(new ScrollingMovementMethod());
+        obj = ("" + formatString(pref.getString("blockchain", "{}")) + "");
         display.setText("" + formatString(pref.getString("blockchain", "{}")) + "");
+        try {
+            Writer output;
+            File file = new File(pathSDCard + disaster + ".json");
+            output = new BufferedWriter(new FileWriter(file));
+            output.write(obj);
+            output.close();
+            Toast.makeText(getApplicationContext(), "JSON saved", Toast.LENGTH_LONG).show();
+
+
+        } catch (Exception e) {
+            Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
     }
 
     public static String formatString(String text){
